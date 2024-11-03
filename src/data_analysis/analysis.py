6b5a8034a51dbaaebda09cb40384dbe7a5d7be38
daniampr <daniel.amirian01@estudiant.upf.edu>
2024-11-04 00:10:43 +0100

    Added chatbot first version and fixed bugs

--- src/data_analysis/analysis.py
+++ src/data_analysis/analysis.py
@@ -1,3 +1,16 @@
 '''
-Functions to 
+
+
 '''
+import pandas as pd
+
+
+def load_csv(file_path):
+    '''
+    Load a CSV file into a pandas DataFrame.
+    Parameters:
+        file_path (str): The path to the CSV file.
+    Returns:
+        pd.DataFrame: The DataFrame containing the data from the CSV file.
+    '''
+    return pd.read_csv(file_path)
---
+++ src/utils/helpers.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+DATA = pd.read_csv('../files/dataset/full_dataset.csv')
